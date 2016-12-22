f4bc024cdd965dd3d18e10015f4c8094d0082690
Swen Kooij <photonios@outlook.com>
2016-12-22 23:01:12 +0200

    Added CLI option for port number

--- cfrweb/__main__.py
+++ cfrweb/__main__.py
@@ -1,8 +1,22 @@
+import argparse
 import logging
 
 import aiohttp
 
 from cfrweb import routes
+
+
+def _parse_arguments() -> argparse.Namespace:
+    """Parses the command line arguments."""
+
+    parser = argparse.ArgumentParser(description='cfr.ninja web server')
+
+    parser.add_argument('--port',
+                        default=8080,
+                        type=int,
+                        help='Port to listen on.')
+
+    return parser.parse_args()
 
 
 def main():
@@ -11,6 +25,7 @@ def main():
     logging.basicConfig(level=logging.DEBUG)
 
     app = aiohttp.web.Application()
+    arguments = _parse_arguments()
 
     for methods, route, handler in routes.get():
         resource = app.router.add_resource(route)
@@ -18,7 +33,7 @@ def main():
         for method in methods:
             resource.add_route(method, handler)
 
-    aiohttp.web.run_app(app, port=80)
+    aiohttp.web.run_app(app, port=arguments.port)
 
 
 if __name__ == '__main__':
