9f27f0426da0d2e70a9036db78339b35e6a000d8
Swen Kooij <photonios@outlook.com>
2016-12-23 17:38:55 +0200

    Re-arranged settings

--- config/common.py
+++ config/common.py
@@ -5,13 +5,14 @@ DEBUG = False
 ROOT_DIR = os.path.join(os.path.dirname(__file__), '..')
 PROJECT_DIR = os.path.join(os.path.dirname(__file__), '../cfrweb')
 STATIC_FILES_DIR = os.path.join(PROJECT_DIR, 'assets')
-TEMPLATES_DIR = os.path.join(ROOT_DIR, 'cfrweb/templates')
+
 CACHE_TIMEOUT = collections.OrderedDict([
     ('image/*', 604800),
     ('text/*', 300),
     ('*', 300)
 ])
 
+TEMPLATES_DIR = os.path.join(ROOT_DIR, 'cfrweb/templates')
 TEMPLATES_DEFAULT_CONTEXT = {
     'meta': {
         'description': 'Real-time updates on trains in Romania.',
