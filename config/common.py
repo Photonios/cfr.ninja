bb551efcf79b5add91c0134e144d7874add9a612
Swen Kooij <photonios@outlook.com>
2016-12-23 15:05:41 +0200

    Added production configuration

--- config/common.py
+++ config/common.py
@@ -1,6 +1,8 @@
 import os
 
 
+DEBUG = False
 ROOT_DIR = os.path.join(os.path.dirname(__file__), '..')
 STATIC_FILES_DIR = os.path.join(ROOT_DIR, 'assets')
 TEMPLATES_DIR = os.path.join(ROOT_DIR, 'cfrweb/templates')
+CACHING_TIME = 300
